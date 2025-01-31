94e014451350f47358341f87199304660fa2d031
Andrii Prykhodko <andriichello@gmail.com>
2025-01-31 23:17:17 +0200

    Use BaseQuery in BaseModel

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use App\Models\Interfaces\IdentifiesCompanyInterface;
 use App\Models\Traits\IdentifiesCompany;
+use App\Queries\BaseQuery;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder as DatabaseBuilder;
 use Illuminate\Support\Carbon;
 
 /**
@@ -13,9 +15,21 @@ use Illuminate\Support\Carbon;
  * @property int $id
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
+ *
+ * @method static BaseQuery query()
  */
 class BaseModel extends Model implements
     IdentifiesCompanyInterface
 {
     use IdentifiesCompany;
+
+    /**
+     * @param DatabaseBuilder $query
+     *
+     * @return BaseQuery
+     */
+    public function newEloquentBuilder($query): BaseQuery
+    {
+        return new BaseQuery($query);
+    }
 }

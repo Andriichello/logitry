744065ab4d7d3de04f95e94403e1b649c6c7e233
Andrii Prykhodko <andriichello@gmail.com>
2025-02-01 18:19:07 +0200

    Create basic GET /api/me endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,20 @@
 <?php
 
 use App\Http\Controllers\Auth as Auth;
+use App\Http\Resources\User\MeResource;
+use App\Http\Responses\ApiResponse;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['as' => 'api.'], function () {
     Route::get('/example', function () {
-        return response()->json(['message' => 'OK']);
+        return ApiResponse::ok();
     });
 
     Route::post('/login', [Auth\LoginController::class, 'login']);
 });
 
+Route::group(['as' => 'api.', 'middleware' => ['auth:sanctum']], function () {
+    Route::get('/me', function () {
+        return ApiResponse::ok(['me' => new MeResource(request()->user())]);
+    });
+});
